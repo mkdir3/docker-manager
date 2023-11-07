ec9da39004ed755b1970056fb18ceaac63a9e494
mkdir3 <tenderpanini@gmail.com>
2023-11-07 22:31:34 +0100

    feat(): handling unknown projects, refactor, project info, loader
    - KO launching projects

--- pkg/utils/paths.go
+++ pkg/utils/paths.go
@@ -7,6 +7,29 @@ import (
 	"strings"
 )
 
+func PathExists(path string) bool {
+	loader := NewLoader()
+	loader.Start("Resolving paths from projects.json... ")
+
+	resolvedPath, err := ResolveHomeDir(path)
+	if err != nil {
+		loader.Stop()
+		fmt.Printf("Error resolving path: %v\n", err)
+		return false
+	}
+
+	_, err = os.Stat(resolvedPath)
+	loader.Stop()
+	return !os.IsNotExist(err)
+}
+
+func PathChecker(path string) {
+	if !PathExists(path) {
+		fmt.Printf("The path %s does not exist or is not accessible.\n", path)
+		return
+	}
+}
+
 func ResolveHomeDir(path string) (string, error) {
 	if !strings.HasPrefix(path, "~") {
 		return path, nil
@@ -19,13 +42,14 @@ func ResolveHomeDir(path string) (string, error) {
 }
 
 func GetComposeFilePath(projectDir string) (string, error) {
-	ymlPath := projectDir + "/docker-compose.yml"
-	if _, err := os.Stat(ymlPath); err == nil {
-		return ymlPath, nil
+	fileNames := []string{"docker-compose.yml", "docker-compose.yaml"}
+
+	for _, fileName := range fileNames {
+		fullPath := projectDir + "/" + fileName
+		if PathExists(fullPath) {
+			return fullPath, nil
+		}
 	}
-	yamlPath := projectDir + "/docker-compose.yaml"
-	if _, err := os.Stat(yamlPath); err == nil {
-		return yamlPath, nil
-	}
-	return "", fmt.Errorf("no docker-compose file found in %s", projectDir)
+
+	return "", fmt.Errorf("no docker-compose file found in %s 🤔", projectDir)
 }
